2d198651063ea413f2fb800f502a410f2d026c33
Scott Hill <scott289@gmail.com>
2011-10-21 11:36:03 -0700

    Refining it, adding main and init.

--- pydbutil/DbProfile.py
+++ pydbutil/DbProfile.py
@@ -1,9 +1,22 @@
 #! /usr/bin/python
 
-ENVS = ['DEV', 'UAT', 'PROD']
-
 # The dict of DbProfile objects
 DB = {}
+
+ENVS = ['DEV', 'UAT', 'PROD']
+
+# The keys defining a DbProfile values...
+_db_data_keys = (
+     'dbname', 'env', 'boxname', 'path')
+
+_db_data = (
+    ('RDxETL',    'DEV', 'usfshwssql104', r'D:\Something'),
+    ('RDxETL',    'UAT', 'usfshwssql094', r'D:\Something'),
+    ('RDxETL',    'PROD','usfshwssql077', r'D:\Something'),
+    ('RDxReport', 'UAT', 'usfshwssql089', r'D:\Something'),
+    ('RDxReport', 'PROD','usfshwssql084', r'D:\Something'),
+    ('RDxReport', 'DEV', 'usfshwssql104\RIGHTSDEV_2', r'D:\Something') 
+)
 
 class DbProfile:
     def __init__(self, dbname, env, boxname, path):
@@ -20,22 +33,24 @@ class DbProfile:
         return (self.dbname, self.env)
 
 
-# The keys defining a DbProfile values...
-_db_data_keys = (
-     'dbname', 'env', 'boxname', 'path')
+def __init__(self):
+    #self.data = data
+    print 'Initializing', self.__name__
+    for db_data_rec in _db_data:
+        db_dict = dict(zip(_db_data_keys, db_data_rec))
+        db = DbProfile(**db_dict)  # **=unpack
+        #add as a dict, keyed by tuple of (dbname, env)
+        DB[db.get_key()] = db
 
-_db_data = (
-    ('RDxETL',    'DEV', 'usfshwssql104', r'D:\Something'),
-    ('RDxETL',    'UAT', 'usfshwssql094', r'D:\Something'),
-    ('RDxETL',    'PROD','usfshwssql077', r'D:\Something'),
-    ('RDxReport', 'UAT', 'usfshwssql089', r'D:\Something'),
-    ('RDxReport', 'PROD','usfshwssql084', r'D:\Something'),
-    ('RDxReport', 'DEV', 'usfshwssql104\RIGHTSDEV_2', r'D:\Something') 
-)
-for db_data_rec in _db_data:
-    db_dict = dict(zip(_db_data_keys, db_data_rec))
-    db = DbProfile(**db_dict)  # **=unpack
-    #add as a dict, keyed by tuple of (dbname, env)
-    DB[db.get_key()] = db
+if __name__ == "__main__":
+    print 'Test profile access...'
+    print DB
+    sys.exit(main())
+
+#for db_data_rec in _db_data:
+#    db_dict = dict(zip(_db_data_keys, db_data_rec))
+#    db = DbProfile(**db_dict)  # **=unpack
+#    #add as a dict, keyed by tuple of (dbname, env)
+#    DB[db.get_key()] = db
 
 

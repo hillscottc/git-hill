1a5b80f2a174040630912903a50505cb419a7fd7
Scott Hill <scott289@gmail.com>
2011-10-29 08:27:35 -0700

    ok

--- pydbutil/DbProfile.py
+++ pydbutil/DbProfile.py
@@ -5,8 +5,8 @@ Can takes a csv file as input.
 
 Usage: 
     ./DbProfile.py -i DbSet.data.csv
+    ./DbProfile.py -d "RDxETL PROD usfshwssql077 D:\Something"
 Args:
-    -i: infile (csv format)
 Returns:
 Raises:
 """
@@ -28,12 +28,16 @@ class DbProfile():
     Keys = ('dbname', 'env', 'boxname', 'path')
 
     def __init__(self, dbname='RDxETL', env='UAT', boxname='usfshwssql104',
-                 path='somepath'):
-        self.dbname = dbname
-        self.env = env
-        self.boxname = boxname
-        self.path = path
-
+                 path='somepath', tup=None):
+        if tup:
+            db_dict = dict(zip(self.Keys, tup))
+            self.__init__(**db_dict)
+        else:
+            self.dbname = dbname
+            self.env = env
+            self.boxname = boxname
+            self.path = path            
+    
     def __str__(self):
         return self.dbname + ' ' + self.env + ' ' + self.boxname + ' ' + self.path
     def __repr__(self):
@@ -41,12 +45,6 @@ class DbProfile():
     def get_key(self):
         # key is a tuple
         return (self.dbname, self.env)
-
-
-def get_profile_from_tuple(tup):
-    """ Create a profile from tuple of values."""
-    db_dict = dict(zip(DbProfile.Keys, tup))
-    return DbProfile(**db_dict)
 
 
 class DbSet():
@@ -68,7 +66,6 @@ class DbSet():
         return self.DB[(dbname, env)]
 
     def __str__(self):
-        #s = ''
         return str([str(dbprofile) for dbprofile in self.DB])  
     def __repr__(self):
         return str(self.__str__())
@@ -79,11 +76,12 @@ def main(argv=None):
          argv = sys.argv
     try:
         try:
-            opts, args = getopt.getopt(argv[1:], "hi:", ["help", "infile="])
+            opts, args = getopt.getopt(argv[1:], "hi:d:", ["help", "infile=","data="])
         except getopt.error, msg:
             raise Usage(msg)
 
         infile = None
+        data = None
 
         for opt, arg in opts :
             if opt in ("-h", "--help"):
@@ -91,19 +89,16 @@ def main(argv=None):
                 sys.exit(0)
             elif opt in ("-i", "--infile"):
                 infile = arg
+                if not os.path.isfile(infile) :
+                    raise Usage("Invalid -i '{}'".format(infile))
+                else:
+                    print 'Creating DbSet from infile -- {}'.format(DbSet(cvsfile=infile))
+            elif opt in ("-d", "--data"):
+                data = tuple(arg.split())
+                print 'Creating dbprofile from d -- {}'.format(DbProfile(tup=data))      
 
-        print 'Create dbprofile from default -- ', DbProfile()
+        print 'Creating a default dbprofile -- ', DbProfile()
 
-        print 'Create dbprofile from tuple -- {}'.format(
-            get_profile_from_tuple(('RDxETL', 'PROD', 'usfshwssql077', r'D:\Something')))
-
-        if infile:
-            if not os.path.isfile(infile) :
-                raise Usage("Invalid -i '{}'".format(infile))
-            else:
-                print 'Create DbSet -- {}'.format(DbSet(cvsfile=infile))
-        
-        
     except Usage, err:
         print >>sys.stderr, "Sorry, invalid options. For help, use --help"
         print >>sys.stderr, "Other errors:",err.msg

ab83d30bff55168b22045b35103a905a7dda2cd9
Scott Hill <scott289@gmail.com>
2011-11-05 07:23:45 -0700

    comments.

--- copy-to-dropbox.sh
+++ copy-to-dropbox.sh
@@ -2,16 +2,15 @@
 source=~/git-hill
 bak=$source-bak
 targ=~/Dropbox/${bak##*/}
-#echo $source $bak $targ
+echo Copy files from $source to $bak
 rm -rf $bak
 mkdir $bak
 cp -rf $source/* $bak/
+echo Remove .git folders from $bak
 find $bak/ -name ".git" -type d -exec rm -rf {} \;
 rm -rf $targ
+echo Move $bak to $targ
 mv -f $bak $targ
-ls -al $source
-echo COPIED TO ... $source
-echo $targ
-ls -al $targ
-echo COPIED FROM --$source 
-echo TO          --$targ
+echo Complete.
+
+

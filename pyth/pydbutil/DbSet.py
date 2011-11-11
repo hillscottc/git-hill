2f751f12943ab92aa8c34dfe583e9db7c7bad4d5
Scott Hill <scott289@gmail.com>
2011-11-11 13:27:28 -0800

    ok

--- pyth/pydbutil/DbSet.py
+++ pyth/pydbutil/DbSet.py
@@ -16,38 +16,43 @@ class Usage(Exception):
     def __init__(self, msg):
         self.msg = msg
 
-class DbSet():
+class DbSet(object):
     """Manages database profiles. Takes a csv file as input.
-    
     Usage:
     >>> dbset = DbSet('input/DbSet.data.csv')
-    
+    >>> print len(dbset)
+    12
     """
     DB = []
     def __init__(self, cvsfile=None, dbprofiles=[],
                  regex='Data Source=(Usfshwssql\w+);Initial Catalog=(RDx\w+);'):
         self.DB = []
         self.regex = regex
-        if cvsfile:
-            dr = csv.DictReader(open(cvsfile, 'rb'), delimiter=',', quotechar="'")
-            self.__init__(dbprofiles=[DbProfile(**row) for row in dr])
-        else:
-            self.DB = [db for db in dbprofiles]
-            #print 'Loaded {} profile(s).'.format(len(self.DB))
+        self.cvsfile = cvsfile
+
+    def set_cvsfile(self, value):
+        dr = csv.DictReader(open(value, 'rb'), delimiter=',', quotechar="'")
+        dbprofiles = [DbProfile(**row) for row in dr]
+        self.DB = [db for db in dbprofiles]
+        self._cvsfile = value
+        
+    def get_cvsfile(self):
+        return self._cvsfile
+
+    cvsfile = property(get_cvsfile, set_cvsfile)
 
     def __len__(self):
         return len(self.DB)
 
     def get_profile_by_attribs(self, aDict):
-        """Does given dict of attib-vals match with self data?
+        """Does given dict of attrib-vals match with self data?
         
         Usage:
         >>> dbset = DbSet('input/DbSet.data.csv')
         
         Have a training RDxETL?
-        >>> print dbset.get_profile_by_attribs(dict(env='training', dbname='RDxETL'))
+        #>>> print dbset.get_profile_by_attribs(dict(env='training', dbname='RDxETL'))
         None
-        
         """
         for db in self.DB:
             if db.match_attrib(aDict):
@@ -62,7 +67,7 @@ class DbSet():
 
 if __name__ == "__main__":
     import doctest
+    doctest.testmod(verbose=True)
     doctest.testfile("tests/test_DbSet.txt")
-    doctest.testmod(verbose=True)    
     sys.exit(0)
 

73fbfbb3d04379a9c733599e61f4cdcda6c5c27d
Scott Hill <scott289@gmail.com>
2012-01-16 08:12:45 -0800

    ok

---
+++ pyth/pydbutil/MatchedConfig.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python
+
+import sys
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+class MatchedConfig(object):
+
+    def __init__(self, mtype=None, before=None, linenum=None, after=None,
+                 newname=None):
+        self.mtype = mtype
+        self.before = before
+        self.linenum = linenum
+        self.after = after
+        self.newname = newname
+        
+    
+    def is_correct(self):
+        """ Does before match before?"""     
+        if self.before.upper() == self.after.upper() :
+            return True
+        else :
+            return False     
+        
+    def __str__(self):
+        return '{} {} {}'.format(self.before, self.linenum, self.after)
+
+    def __repr__(self):
+        return str(self.__str__())
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
+    sys.exit(0)
+    #sys.exit(main())
+
+
+    

951be83aa028aec6a30cad727d43ea95a0a1aeca
Scott Hill <scott289@gmail.com>
2012-03-16 09:53:04 -0700

    config for uat

--- pyth/pydbutil/Configure.py
+++ pyth/pydbutil/Configure.py
@@ -23,17 +23,17 @@ APPS = ('CARL', 'CART', 'Common', 'CPRS','CRA', 'CTX', 'D2',
         'DRA', 'ELS', 'GDRS', 'MP', 'PartsOrder', 'R2')
 
 _dbs = (('RDxETL', 'USHPEPVSQL363'), ('RDxReport', 'USHPEPVSQL347'))
-_envs = ('prod', )
+_envs = ('uat', )
 
 DBSET = DbSet(DbProfile.create_profiles(envs=_envs, apps=APPS, dbs=_dbs))
 
+_ftp_root = 'USHPEWVAPP204'
 
-_ftp_root = 'USHPEWVAPP086'
 _log_path = os.path.join('D:', 'RDx', 'ETL', 'logs')
 _smtp_server = 'usush-maildrop.amer.umusic.net'
 _to = 'ar.umg.rights.dev@hp.com, Scott.Hill@umgtemp.com'
 _from = 'RDx@mgd.umusic.com'
-_subj = 'RDxAlert Message'
+_subj = 'RDxAlert Message ' + _envs[0]
 _configs = (ConfigObj('LOG_A', '<file value="(.+)"', _log_path),
                 ConfigObj('LOG_B', '"file" value="(.+)"', _log_path),
                 ConfigObj('DB', 'Data Source=(.+);Initial Catalog=(RDx\w+);', ''),

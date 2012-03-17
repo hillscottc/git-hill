776d3902c035a2689fbb2e065f0a128d7a5bb506
Scott Hill <scott289@gmail.com>
2012-03-17 13:58:48 -0700

    improve err mess

--- pyth/pydbutil/FileUtils.py
+++ pyth/pydbutil/FileUtils.py
@@ -94,6 +94,7 @@ def change_root(filepath, old_root, new_root='work', ensure=False):
     except Exception as inst:
         #print type(inst)     # the exception instance
         #print inst.args      # arguments stored in .args
+        print inst
         raise MyError('{0}, {1}, {2}'.format(old_root, new_root, filepath))
 
     return outfilename

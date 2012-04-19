1f876043799c656701e4a4b71b74f783bb1738a2
Scott Hill <scott.cla.hill@hp.com>
2012-04-19 08:44:50 -0700

    Ok.

--- batch/sqlrun.bat
+++ batch/sqlrun.bat
@@ -1,20 +1,18 @@
 ECHO off
-rem Usage: sqlrun.bat ushpepvsql409 "SELECT TOP 5 * FROM dbo.Box;"
+rem Usage: sqlrun.bat RDxETL "SELECT TOP 5 * FROM dbo.Box;"
 
-SET BOX=%1
+
+SET DB=%1
 SET QUERY=%2
 
 SET USER=rdxuserdev
 SET PASS=01Music
-SET DB=RDxETL
 
-REM - RDxReport db
-IF "%BOX%" == "USHPEPVSQL435" (
-	SET USER=ScottH
-	SET PASS=H1ll_Sc0!
-	SET DB=RDxReport
-)
+if %DB%==RDxETL SET BOX=USHPEPVSQL409
+if %DB%==RDxReport SET BOX=USHPEPVSQL435
+
 
 ECHO on
 
 sqlcmd -S %BOX%\%BOX% -d %DB% -U %USER% -P %PASS% -W -Q %QUERY%
+

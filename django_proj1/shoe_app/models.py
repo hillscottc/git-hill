a3096e93e4108550e8ef23261b18de8c5469bab3
Scott Hill <scott289@gmail.com>
2012-05-31 23:12:40 -0700

    str meths

--- django_proj1/shoe_app/models.py
+++ django_proj1/shoe_app/models.py
@@ -2,18 +2,21 @@ from django.db import models
 import datetime
 from django.utils import timezone
 from django import forms
-from django.forms import ModelForm, Textarea
+from django.forms import ModelForm
+from django.forms import Textarea
 
 
 class ShoeColor(models.Model):
     name = models.CharField("color", max_length=12)
-
+    def __str__(self):
+        return self.name
     def __unicode__(self):
         return self.name
 
 class ShoeType(models.Model):
     name = models.CharField(max_length=12)
-
+    def __str__(self):
+        return self.name
     def __unicode__(self):
         return self.name
 
@@ -22,13 +25,12 @@ class Shoe(models.Model):
     shoeColor = models.ForeignKey(ShoeColor, blank=True, null=True)
     shoeType = models.ForeignKey(ShoeType, blank=True, null=True)
     add_date = models.DateTimeField('date added', null=True)
-
     class Meta:
         ordering = ["-name"]
-
+    def __str__(self):
+        return self.name
     def __unicode__(self):
         return self.name
-
     def was_added_recently(self):
         return self.add_date >= timezone.now() - datetime.timedelta(days=1)
 
@@ -40,3 +42,7 @@ class ShoeForm(ModelForm):
 class Tag(models.Model):
     name = models.CharField(max_length=64, unique=True)
     shoes = models.ManyToManyField(Shoe)
+    def __str__(self):
+        return self.name
+    def __unicode__(self):
+        return self.name

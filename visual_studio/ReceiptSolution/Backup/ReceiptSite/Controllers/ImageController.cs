2cf49cb01356357f4286c884347e6deaf5dde5b6
Scott Hill <scott289@gmail.com>
2012-09-24 09:07:22 -0700

    affect of opeing it in vs 2012

---
+++ visual_studio/ReceiptSolution/Backup/ReceiptSite/Controllers/ImageController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ReceiptSite.Models;
+
+namespace ReceiptSite.Controllers
+{ 
+    public class ImageController : Controller
+    {
+        private ReceiptsEntities db = new ReceiptsEntities();
+
+        //
+        // GET: /Image/
+
+        public ViewResult Index()
+        {
+            return View(db.ImageBanks.ToList());
+        }
+
+        //
+        // GET: /Image/Details/5
+
+        public ViewResult Details(int id)
+        {
+            ImageBank imagebank = db.ImageBanks.Find(id);
+            return View(imagebank);
+        }
+
+        //
+        // GET: /Image/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Image/Create
+
+        [HttpPost]
+        public ActionResult Create(ImageBank imagebank)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ImageBanks.Add(imagebank);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(imagebank);
+        }
+        
+        //
+        // GET: /Image/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            ImageBank imagebank = db.ImageBanks.Find(id);
+            return View(imagebank);
+        }
+
+        //
+        // POST: /Image/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(ImageBank imagebank)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(imagebank).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(imagebank);
+        }
+
+        //
+        // GET: /Image/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            ImageBank imagebank = db.ImageBanks.Find(id);
+            return View(imagebank);
+        }
+
+        //
+        // POST: /Image/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            ImageBank imagebank = db.ImageBanks.Find(id);
+            db.ImageBanks.Remove(imagebank);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ visual_studio/ReceiptSolution/Backup1/Models/ReceiptMetadata.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace ReceiptSite.Models
+{
+    // This blank partial class exists only to add the Metadata.
+    [MetadataType(typeof(ReceiptMetadata))]
+    public partial class Receipt { }
+
+    public class ReceiptMetadata
+    {
+        [Required]
+        public int id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("The text to write")]
+        public string TextData { get; set; }
+
+        [Required]
+        [DisplayName("Modified image")]
+        public Nullable<int> ReceiptImageId { get; set; }
+
+        [Required]
+        [DisplayName("Blank image")]
+        public Nullable<int> BlankImageId { get; set; }
+
+        [Required]
+        public Nullable<bool> Active { get; set; }
+
+        [Required]
+        public Nullable<System.DateTime> CreateDate { get; set; }
+
+    }
+}
